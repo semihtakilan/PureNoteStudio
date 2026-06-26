343b3e0143d897228b45bf251d5de1226acda17b
SemihTAKILAN <semihtakilan16@gmail.com>
2026-06-26 15:10:37 +0300

    Initial Commit

---
+++ PureNoteStudio/PureNoteStudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PureNoteStudioApp.swift
+//  PureNoteStudio
+//
+//  Created by Semih TAKILAN on 26.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct PureNoteStudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
